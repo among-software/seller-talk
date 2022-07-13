07bd0bd0c8ca85d785273268656d5235cb7834b9
kht038 <kht038@naver.com>
2022-07-13 14:10:23 +0900

    🎨 update requirements

--- controller/related_controller.py
+++ controller/related_controller.py
@@ -3,34 +3,36 @@ import json
 from services.contraction import search_volume
 from services import total_product
 from services.detail_category import category_list
+from services.related import web_doc
 
 
 def controller(keyword):
-    search_volume_data = search_volume.search_volume(keyword)
-    response = []
-
-    for relation_keyword_stat in search_volume_data['keywordList'][0:3]:
-        pc_query_count = relation_keyword_stat['monthlyPcQcCnt']
-        if pc_query_count == '< 10':
-            pc_query_count = 9
-        mo_query_count = relation_keyword_stat['monthlyMobileQcCnt']
-        if mo_query_count == '< 10':
-            mo_query_count = 9
-        total_num = total_product.get_total_product(keyword)
-        competitive_strength = round(total_num / (pc_query_count + mo_query_count), 2)
-        category = json.loads(category_list.crawling(relation_keyword_stat['relKeyword']))['categories'][0]
-
-        response.append({
-            'keyword': relation_keyword_stat['relKeyword'],
-            'pc': pc_query_count,
-            'mobile': mo_query_count,
-            'items': total_num,
-            'competition': competitive_strength,
-            'category': category['title'][-1],
-            'classification': {
-                'class': '쇼핑성',
-                'percent': 83
-            }
-        })
-
-    return json.dumps(response, ensure_ascii=False)
+    # search_volume_data = search_volume.search_volume(keyword)
+    # response = []
+    #
+    # for relation_keyword_stat in search_volume_data['keywordList'][0:3]:
+    #     pc_query_count = relation_keyword_stat['monthlyPcQcCnt']
+    #     if pc_query_count == '< 10':
+    #         pc_query_count = 9
+    #     mo_query_count = relation_keyword_stat['monthlyMobileQcCnt']
+    #     if mo_query_count == '< 10':
+    #         mo_query_count = 9
+    #     total_num = total_product.get_total_product(keyword)
+    #     competitive_strength = round(total_num / (pc_query_count + mo_query_count), 2)
+    #     category = json.loads(category_list.crawling(relation_keyword_stat['relKeyword']))['categories'][0]
+    #
+    #     response.append({
+    #         'keyword': relation_keyword_stat['relKeyword'],
+    #         'pc': pc_query_count,
+    #         'mobile': mo_query_count,
+    #         'items': total_num,
+    #         'competition': competitive_strength,
+    #         'category': category['title'][-1],
+    #         'classification': {
+    #             'class': '쇼핑성',
+    #             'percent': 83
+    #         }
+    #     })
+    #
+    # return json.dumps(response, ensure_ascii=False)
+    return web_doc.get_naver_web_doc(keyword)
--- services/related/web_doc.py
+++ services/related/web_doc.py
@@ -1,24 +1,18 @@
 # 환경변수 호출
 import requests, os
-from dotenv import load_dotenv
-load_dotenv()
-
-client_id = os.getenv('NAVER_CLIENT_ID')
-client_secret = os.getenv('NAVER_CLIENT_SECRET')
+from bs4 import BeautifulSoup
 
 
 def get_naver_web_doc(keyword):
+    url = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&"
 
-    open_api_url = f"https://openapi.naver.com/v1/search/webkr.json?query={keyword}"
-    headers = {"X-Naver-Client-Id": client_id, "X-Naver-Client-Secret": client_secret,
-               "Content-Type": "application/json"}
-    request = requests.get(open_api_url, headers=headers)
+    request = requests.get(url, params={'query': keyword})
 
     response_code = request.status_code
-    response_message = request.json()
-
+    response_html = request.text
+    keyword_order
     if response_code == 200:
-        return response_message
+        return str(response_message)
     else:
         print("Error Code : " + str(response_code))
         return str(response_code)
